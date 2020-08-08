175895e48d02d1871a4bb98216b345c136a6f8f6
emranweb <emranbd77@gmail.com>
2020-08-08 16:12:15 +0600

    Updates

---
+++ precache-manifest.f1cd5a7ba30c4c26a506ac3358390dc7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b4767177978cefaf837c797b1c2d440",
+    "url": "/emranweb.github.io/rr-ecommerce/index.html"
+  },
+  {
+    "revision": "8b41139e0194b6db4752",
+    "url": "/emranweb.github.io/rr-ecommerce/static/css/main.c6b500f8.chunk.css"
+  },
+  {
+    "revision": "1597b9e386462daaa2d8",
+    "url": "/emranweb.github.io/rr-ecommerce/static/js/2.3a194bca.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/emranweb.github.io/rr-ecommerce/static/js/2.3a194bca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8b41139e0194b6db4752",
+    "url": "/emranweb.github.io/rr-ecommerce/static/js/main.f7fbd4d6.chunk.js"
+  },
+  {
+    "revision": "ac1e1e77e9c77cda4b15",
+    "url": "/emranweb.github.io/rr-ecommerce/static/js/runtime-main.24e5714b.js"
+  }
+]);
