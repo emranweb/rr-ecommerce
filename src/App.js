81b286658f2128212746f244af2e525802dea7d3
emranweb <emranbd77@gmail.com>
2020-08-17 19:53:40 +0600

    hats page update

--- src/App.js
+++ src/App.js
@@ -5,26 +5,6 @@ import {Route, Switch, Link} from "react-router-dom";
 import ShopPage from "./pages/shop/shop.component.js";
 
 
-function Hatpage (){
-  return (
-    <div>
-     <h1>Helo World</h1>  
-    </div>
-    
-  )
-}
-
-
-
-
-function TopicDetails (props){
-  return (
-    <div>
-      <h1>Helo World: {props.match.params.topicId}</h1>
-    </div>
-  )
-}
-
 
 
 function App() {
@@ -33,7 +13,7 @@ function App() {
       
       <Switch>
          <Route exact  path="/" component={Homepage}></Route>
-         <Route  path="/hats" component={Hatpage}></Route>
+         <Route  path="/hats" component={ShopPage}></Route>
          <Route  path="/shop" component={ShopPage}></Route>
       </Switch>
 
--- src/components/menu-item/menu-item.component.js
+++ src/components/menu-item/menu-item.component.js
@@ -7,8 +7,8 @@ function MenuItem (props){
     return (
             <div className="menu-item" style={{backgroundImage:`url(${props.imageUrl})`}} onClick={()=>props.history.push(`${props.match.url}${props.title}`)}>
                 <div className="content">
-                <h3 className="title">{props.title}</h3>
-                <span className="sub-title">SHOP NOW</span>
+                   <h3 className="title">{props.title}</h3>
+                   <span className="sub-title">SHOP NOW</span>
                 </div>
             </div>
     )
